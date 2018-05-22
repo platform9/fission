aba3842579c730d00d9e778b588cf8782e07fe25
Vishal Biyani <vishal@infracloud.io>
2018-05-22 17:34:21 +0530

    Adding draft cut of working upload

--- fission/main.go
+++ fission/main.go
@@ -61,6 +61,10 @@ func getFissionAPIVersion(apiUrl string) (string, error) {
 }
 
 func getServerUrl() string {
+	return getApplicationUrl("application=fission-api")
+}
+
+func getApplicationUrl(selector string) string {
 	var serverUrl string
 	// Use FISSION_URL env variable if set; otherwise, port-forward to controller.
 	fissionUrl := os.Getenv("FISSION_URL")
@@ -68,7 +72,7 @@ func getServerUrl() string {
 		fissionNamespace := getFissionNamespace()
 		kubeConfig := getKubeConfigPath()
 		localPort := setupPortForward(
-			kubeConfig, fissionNamespace, "application=fission-api")
+			kubeConfig, fissionNamespace, selector)
 		serverUrl = "http://127.0.0.1:" + localPort
 	} else {
 		serverUrl = fissionUrl
