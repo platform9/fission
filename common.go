53d7ae5253e592832aa843c19c874a01a2c9fb7c
Bhavin Gandhi <bhavin7392@gmail.com>
2019-01-26 15:30:41 +0530

    Consider Pod Phase in IsReadyPod
    - This makes sure if the Pod is in Running state
    - Checks if an IP address is allocated to the Pod
    
    Signed-off-by: Bhavin Gandhi <bhavin7392@gmail.com>

--- common.go
+++ common.go
@@ -120,9 +120,20 @@ func IsReadyPod(pod *apiv1.Pod) bool {
 		return false
 	}
 
+	// pod is not in Running Phase. It can be in Pending,
+	// Succeeded, Failed, Unknown
+	if pod.Status.Phase != apiv1.PodRunning {
+		return false
+	}
+
 	// pod is in "Terminating" status if deletionTimestamp is not nil
 	// https://github.com/kubernetes/kubernetes/issues/61376
 	if pod.ObjectMeta.DeletionTimestamp != nil {
+		return false
+	}
+
+	// pod does not have an IP address allocated to it yet
+	if pod.Status.PodIP == "" {
 		return false
 	}
 
